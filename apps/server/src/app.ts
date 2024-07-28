20b9a61e877a90deef3695911ca259c1afbaf957
Haku <hakudevtw@gmail.com>
2024-07-28 21:29:38 +0800

    refactor: refactor package structure and server demo

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -7,12 +7,4 @@ const app = express();
 
 app.use(morganMiddleware);
 
-logger.error('logger error message');
-logger.warn('logger warn message');
-logger.info('logger info message');
-logger.http('logger http message');
-logger.verbose('logger verbose message');
-logger.debug('logger debug message');
-logger.silly('logger silly message');
-
-app.listen(PORT, () => console.log(`Server is listening on ${PORT} ...`));
+app.listen(PORT, () => logger.info(`Server is listening on ${PORT} ...`));
