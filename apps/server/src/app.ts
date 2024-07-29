ffd4450085a0d30ab0743154dbe7256c736b5033
Haku <hakudevtw@gmail.com>
2024-07-29 22:10:30 +0800

    chore: adjust server settings

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,4 +1,5 @@
 import { logger, morganMiddleware } from '@repo/logger';
+import bodyParser from 'body-parser';
 import express from 'express';
 
 const PORT = process.env.PORT ?? 3000;
@@ -6,5 +7,7 @@ const PORT = process.env.PORT ?? 3000;
 const app = express();
 
 app.use(morganMiddleware);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(PORT, () => logger.info(`Server is listening on ${PORT} ...`));
