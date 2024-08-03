cb7a135f6f765be8ca832f343d94f8b3478cc5ba
Haku <hakudevtw@gmail.com>
2024-08-03 17:48:42 +0800

    chore: remove server ( config later )

--- apps/server/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  env: { node: true },
-  extends: ["@repo/eslint-config/library.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
