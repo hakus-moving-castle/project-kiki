bb881adf9c0a0c19bc7a4f20e0d7e42469294235
Haku <hakudevtw@gmail.com>
2024-10-05 15:29:40 +0800

    feat: remove serialization in service layer

--- apps/services/service-users/src/users/users.service.ts
+++ apps/services/service-users/src/users/users.service.ts
@@ -11,66 +11,58 @@ export class UsersService {
 		private readonly drizzleService: DrizzleService<typeof usersSchema>,
 	) {}
 
-	async getUsers() {
-		const { password, ...rest } = getTableColumns(usersSchema.users);
-		const users = await this.drizzleService.db
-			.select({ ...rest })
-			.from(usersSchema.users);
+	async getUsers(): Promise<UserSelect[]> {
+		const users = await this.drizzleService.db.select().from(usersSchema.users);
 		return users;
 	}
 
-	async getUserById(id: UserSelect["id"]) {
-		const { password, ...rest } = getTableColumns(usersSchema.users);
+	async getUserById(id: UserSelect["id"]): Promise<UserSelect> {
 		const user = await this.drizzleService.db
-			.select({ ...rest })
+			.select()
 			.from(usersSchema.users)
 			.where(eq(usersSchema.users.id, id));
-		return user;
+		// @ts-expect-error throw error if user is not found
+		return user[0];
 	}
 
-	async getUserByEmail(email: UserSelect["email"]) {
+	async getUserByEmail(email: UserSelect["email"]): Promise<UserSelect> {
 		const { password, ...rest } = getTableColumns(usersSchema.users);
 		const user = await this.drizzleService.db
 			.select({ ...rest })
 			.from(usersSchema.users)
 			.where(eq(usersSchema.users.email, email));
-		return user;
+		// @ts-expect-error throw error if user is not found
+		return user[0];
 	}
 
-	async getUserWithPasswordByEmail(email: UserSelect["email"]) {
-		const { ...rest } = getTableColumns(usersSchema.users);
-		const user = this.drizzleService.db
-			.select({ ...rest })
-			.from(usersSchema.users)
-			.where(eq(usersSchema.users.email, email));
-		return user;
-	}
-
-	async createUser(user: UserInsert) {
-		const { password, ...rest } = getTableColumns(usersSchema.users);
+	async createUser(user: UserInsert): Promise<UserInsert> {
 		const newUser = await this.drizzleService.db
 			.insert(usersSchema.users)
 			.values(user)
-			.returning({ ...rest });
-		return newUser;
+			.returning();
+		// @ts-expect-error throw error if user is not found
+		return newUser[0];
 	}
 
-	async updateUser(id: UserSelect["id"], user: Partial<UserSelect>) {
-		const { password, ...rest } = getTableColumns(usersSchema.users);
+	async updateUser(
+		id: UserSelect["id"],
+		user: Partial<UserSelect>,
+	): Promise<UserSelect> {
 		const updatedUser = await this.drizzleService.db
 			.update(usersSchema.users)
 			.set(user)
 			.where(eq(usersSchema.users.id, id))
-			.returning({ ...rest });
-		return updatedUser;
+			.returning();
+		// @ts-expect-error throw error if user is not found
+		return updatedUser[0];
 	}
 
-	async deleteUser(id: UserSelect["id"]) {
-		const { password, ...rest } = getTableColumns(usersSchema.users);
+	async deleteUser(id: UserSelect["id"]): Promise<UserSelect> {
 		const deletedUser = await this.drizzleService.db
 			.delete(usersSchema.users)
 			.where(eq(usersSchema.users.id, id))
-			.returning({ ...rest });
-		return deletedUser;
+			.returning();
+		// @ts-expect-error throw error if user is not found
+		return deletedUser[0];
 	}
 }
