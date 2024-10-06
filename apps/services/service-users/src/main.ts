6b410342181cf85b838ba50194255557ac9a440b
Haku <hakudevtw@gmail.com>
2024-10-06 12:44:20 +0800

    chore: handle cookies in gateway instead of service

--- apps/services/service-users/src/main.ts
+++ apps/services/service-users/src/main.ts
@@ -1,10 +1,8 @@
 import { INestApplication, VersioningType } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
 
 export const setupApp = (app: INestApplication) => {
-	app.use(cookieParser());
 	app.enableVersioning({
 		type: VersioningType.URI,
 		prefix: false,
