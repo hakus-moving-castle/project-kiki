edfd7452dd7b1a56979264e772df68fe04f9695b
Haku <hakudevtw@gmail.com>
2024-10-06 12:49:05 +0800

    style(services): replace arrow function with function keyword

--- apps/dashboard-api-gateway/src/main.ts
+++ apps/dashboard-api-gateway/src/main.ts
@@ -3,17 +3,18 @@ import { NestFactory } from "@nestjs/core";
 import cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
 
-export const setupApp = (app: INestApplication) => {
+export function setupApp(app: INestApplication) {
 	app.use(cookieParser());
 	app.enableVersioning({
 		type: VersioningType.URI,
 		prefix: false,
 	});
-};
+}
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	setupApp(app);
 	await app.listen(3000);
 }
+
 bootstrap();
--- apps/services/service-users/src/main.ts
+++ apps/services/service-users/src/main.ts
@@ -2,16 +2,17 @@ import { INestApplication, VersioningType } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
-export const setupApp = (app: INestApplication) => {
+export function setupApp(app: INestApplication) {
 	app.enableVersioning({
 		type: VersioningType.URI,
 		prefix: false,
 	});
-};
+}
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	setupApp(app);
 	await app.listen(3000);
 }
+
 bootstrap();
