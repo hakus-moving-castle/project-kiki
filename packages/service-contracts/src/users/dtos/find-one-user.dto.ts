178ded9486308cfcf4dfd450a693cd8d4e0d2e41
Haku <hakudevtw@gmail.com>
2024-10-06 15:32:41 +0800

    feat(service-contracts): add dtos for each controller

---
+++ packages/service-contracts/src/users/dtos/find-one-user.dto.ts
@@ -0,0 +1,7 @@
+import { IsNotEmpty, IsNumber } from "class-validator";
+
+export class FindOneUserDto {
+	@IsNumber()
+	@IsNotEmpty()
+	id!: number;
+}
--- packages/service-contracts/src/users/index.ts
+++ packages/service-contracts/src/users/index.ts
@@ -1,3 +1,7 @@
 export * from "./users.patterns";
+
 export * from "./dtos/create-user.dto";
+export * from "./dtos/find-all-users.dto";
+export * from "./dtos/find-one-user.dto";
 export * from "./dtos/update-user.dto";
+export * from "./dtos/remove-user.dto";
